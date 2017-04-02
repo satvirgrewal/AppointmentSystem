8598ca4d5bd1fe8b3da15a4c47c051fe0455e2ac
vinayakula1 <vinay_akula@tamu.edu>
2017-04-02 18:57:26 -0500

    Update index.php

--- index.php
+++ index.php
@@ -128,7 +128,7 @@
     <!-- Wrapper for slides -->
     <div class="carousel-inner" role="listbox" >
       <div class="item active">
-        <img src="11.jpg" class="img-responsive" alt="Chania" >
+        <img src="12thman.png.jpg" class="img-responsive" alt="Chania" >
       </div>
 
       <div class="item">
