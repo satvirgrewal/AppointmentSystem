b5b451ce6ff16e9d713f0254b4500b57e144f100
vikramlance <vikram.lance@gmail.com>
2017-04-02 20:45:57 -0500

    Update connection.php

--- db/connection.php
+++ db/connection.php
@@ -1,18 +1,12 @@
 <?php
 //---------------------------mysql db connection ------------//
 
-#$hostname='appoinment.cba0t13azydt.us-west-2.rds.amazonaws.com';
 $hostname=getenv('DB_HOSTNAME');
 $username='team25';
 $password='team251234';
 
 
-/* $hostname='localhost';
-$username='ambicacl_vishal';
-$password='@v$02581';
- */
 try {
-		//$dbh = new PDO("mysql:host=$hostname;dbname=ambicacl_appoinment",$username,$password);
         $dbh = new PDO("mysql:host=$hostname;dbname=appoinment",$username,$password);
 //    echo 'Connected to Database<br/>';
     }
