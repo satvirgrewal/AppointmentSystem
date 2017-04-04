18acb4906e1789c16a6fd1dfae98a63a68e22863
vinayakula1 <vinay_akula@tamu.edu>
2017-04-03 20:01:52 -0500

    Update mcg.php

--- mcg.php
+++ mcg.php
@@ -146,7 +146,7 @@
     <div class="container">
       <div class="row">
 		<div class="col-md-12">
-      <p class="navbar-text pull-left" >© 2014 - Site Built By Mr. M.
+      <p class="navbar-text pull-left" >
           
       </p>
 	  </div>
