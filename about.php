1777dc15ecd068e3f9150c3f57cc4ee3a8f75f9b
vinayakula1 <vinay_akula@tamu.edu>
2017-04-03 20:05:34 -0500

    Update about.php

--- about.php
+++ about.php
@@ -140,7 +140,7 @@
     <div class="container">
       <div class="row">
 		<div class="col-md-12">
-      <p class="navbar-text pull-left" >© 2014 - Site Built By Mr. M.
+      <p class="navbar-text pull-left" >
           
       </p>
 	  </div>
