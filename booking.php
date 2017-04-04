48346dbf4b3b518057872e99bd546b172c1387a8
vinayakula1 <vinay_akula@tamu.edu>
2017-04-03 20:31:54 -0500

    Update booking.php

--- booking.php
+++ booking.php
@@ -211,7 +211,7 @@ else
     <div class="container">
       <div class="row">
 		<div class="col-md-12">
-      <p class="navbar-text pull-left" >© 2014 - Site Built By Mr. M.
+      <p class="navbar-text pull-left" >
           
       </p>
 	  </div>
