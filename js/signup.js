9bc993209adbd1c839d3587bf0b1ceae16fac944
vikramlance <vikram.lance@gmail.com>
2017-04-21 00:06:53 -0500

    changes to add page to select college and department

--- js/signup.js
+++ js/signup.js
@@ -2,20 +2,21 @@ $(document).ready(function() {
 
 	$(document).on("click","#signup_save",function()
 		{
-				
+
 				var type=document.getElementById('type').value;
 			    var name=document.getElementById('name').value;
 				var uin=document.getElementById('uin').value;
 				var email=document.getElementById('email').value;
 				var pass1=document.getElementById('pass1').value;
 				var pass2=document.getElementById('pass2').value;
-				
-			
+				var college=document.getElementById('college').value;
+				var department=document.getElementById('department').value;
+
 				//alert(m_id +' '+ p_id +' '+p_unit+' '+p_qty+' '+date+' '+bill_no+' '+party_nm+' '+mfi);
-				
-					$.ajax({ 
+
+					$.ajax({
 							type: "POST",
-							url: "ajax/signup_data.php?type="+type+"&name="+name+"&uin="+uin+"&email="+email+"&pass1="+pass1+"&pass2="+pass2,
+							url: "ajax/signup_data.php?type="+type+"&name="+name+"&uin="+uin+"&email="+email+"&pass1="+pass1+"&pass2="+pass2+"&college="+college+"&department="+department,
 							success: function(data)
 							{
 								//alert(data);
@@ -25,18 +26,20 @@ $(document).ready(function() {
 								$('#e').html(data);
 								$('#p1').html(data);
 								$('#p2').html(data);
-								
-								
+								$('#c').html(data);
+								$('#d').html(data);
+
 								$('#type').val('0');
 								$('#name').val('');
 								$('#uin').val('');
 								$('#email').val('');
 								$('#pass1').val('');
 								$('#pass2').val('');
-								
+								$('#college').val('');
+								$('#department').val('');
 							}
-						});	
-				
+						});
+
 		});
 
 });
