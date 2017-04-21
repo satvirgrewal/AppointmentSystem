5fc8083475183a02c366f922d3da5cfb355623f1
vikramlance <vikram.lance@gmail.com>
2017-04-21 00:05:47 -0500

    resetting the db connections that were used for testing

--- db/connection.php
+++ db/connection.php
@@ -6,6 +6,7 @@ $username='team25';
 $password='team251234';
 
 
+
 try {
         $dbh = new PDO("mysql:host=$hostname;dbname=appoinment",$username,$password);
 //    echo 'Connected to Database<br/>';
