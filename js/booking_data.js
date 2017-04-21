12698a73acc089b503f5bd8259b144964b9ccc66
vikramlance <vikram.lance@gmail.com>
2017-04-21 00:06:23 -0500

    changes to add page to select college and department

--- js/booking_data.js
+++ js/booking_data.js
@@ -1,16 +1,24 @@
 $(document).ready(function() {
 
 	var dl='all';
-								$.ajax({ 
+	// include 'ajax/booking_selection_data.php';
+	// var college = "<?php echo $college ?>";
+	// var department = "<?php echo $department ?>";
+	// var formData = {'college':college,'department':department};
+								$.ajax({
 										type: "POST",
-										url: "ajax/booking_data.php?dl="+dl,
+										url: "ajax/booking_data.php?dl="+dl, //+"&college="+college+"&department="+department,
+										// data: formData,
 										success: function(data)
 										{
 											//alert("gsdfgpjn");
-											$("#example1").html(data);	
-											
-												$("#example1").DataTable();
-										}
+											$("#example1").html(data);
+											console.log(data);
+												// $("#example1").DataTable();
+										},
+										error: function(returnval) {
+										 console.log(returnval);
+									 }
 								});
 
 });
