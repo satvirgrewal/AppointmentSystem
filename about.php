860e9e08ec6d443a4317d0d0f134cff2e1e99831
vikramlance <vikram.lance@gmail.com>
2017-04-30 20:02:25 -0500

    changes for mascot and logo alignment in mobile version

--- about.php
+++ about.php
@@ -9,19 +9,49 @@
   <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
   <link href="main.css" rel="stylesheet" type="text/css">
- 
+
  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
- 
+
   <style>
   .carousel-inner > .item > img,
   .carousel-inner > .item > a > img {
       width: 100%;
 	  HEIGHT:50%;
-     
+
   }
- 
+
+      /* Desktop layout of Howdy Professor */
+    #navbar-mobile{
+      display: block;
+    }
+    h2{
+      font-size: 2em;
+      font-weight: bold;
+    }
+    .navbar-center{
+      margin: 25px 0px 0px 25px;
+    }
+    /* Mobile layout of Howdy Professor uses media queries to adjust layout based on the device size */
+    @media (max-width: 768px){
+      #navbar-mobile{
+        display: none; /* Only show very important content in mobile version*/
+      }
+      h2{
+        font-size: 2em; /* font weight is not bold in mobile version to make it astetically pleasing */
+      }
+      .nav h1 {
+        text-align: center;
+      }
+      .navbar-center{
+        margin-left: 0px;
+        width: 100%;
+        height: 100%;
+      }
+    }
+
+      /* End of style formatting of html page */
   </style>
 </head>
 <body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="60">
@@ -65,20 +95,22 @@
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 			<ul class="nav navbar-nav navbar-left ">
-				<li  ><img src="pro1.jpg" class="img-responsive" alt="Chania"  style="padding-top:20px;padding-bottom:20px;"></li>
+				<li  ><img src="pro1.jpg" class="img-responsive" alt="Chania"  style="padding-top:20px;padding-bottom:20px;" id="navbar-mobile"></li>
 				<li  ><h1 style="padding-left:20px;color:white;">Howdy<br/> Professor</h1></li>
 			</ul>
-            <ul class="nav navbar-nav navbar-center" style="background-color:#009591;margin-top:25px;margin-left:50px;">
+            <ul class="nav navbar-nav navbar-center" style="background-color:#009591;">
       <li class="navigation fnt"><a href="index.php" style="
-    padding-left: 50px;
+    padding-left: 20px;
     padding-right: 50px;
 ">HOME</a></li>
- 
+
       <li  class="navigation fnt"><a href="about.php"style="
     padding-left: 20px;
     padding-right: 50px;
 ">ABOUT US</a></li>
-     <li class="dropdown navigation fnt">
+        <li class="dropdown navigation fnt" style="
+        padding-left: 5px;
+        padding-right: 50px;">
         <a class="dropdown-toggle navigation " data-toggle="dropdown" href="#">DEPARTMENT
         <span class="caret"></span></a>
         <ul class="dropdown-menu ">
@@ -102,8 +134,8 @@
 ">CONTACT</a></li>
 
     </ul>
-	
-	<ul class="nav navbar-nav navbar-right ">
+
+	<ul class="nav navbar-nav navbar-right " id="navbar-mobile">
 				<li  ><img src="pro2.jpg" class="img-responsive" alt="Chania"  style="padding-top:20px;padding-bottom:20px;"></li>
 			</ul>
         </div>
@@ -116,13 +148,13 @@
 <div class="container-fluid bg-main" >
   <div class="row">
    <div class="col-md-12">
-  
+
     <div class="col-sm-4" style="
     padding-top: 100px;
     padding-bottom: 100px;
     padding-left: 100px;
 ">
-      <img id="map" class="img-responsive" src="about.jpg" style="height: 282px;">
+      <img id="map" class="img-responsive" src="about.jpg" style="height: 282px; text-align: center">
     </div>
     <div class="col-sm-8" style="padding-top: 100px;">
       <h2>Our Values</h2><br>
@@ -141,14 +173,14 @@
       <div class="row">
 		<div class="col-md-12">
       <p class="navbar-text pull-left" >
-          
+
       </p>
 	  </div>
 	  </div>
-    
+
     </div>
-    
-    
+
+
   </div>
 
 
